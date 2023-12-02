55ba5ad91134c87691b83ac4d74ce82bdd197622
Win Khant Paing <wkhant047@gmail.com>
2023-12-02 23:57:40 +0630

    updated 2/12/23

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login_signup_ui/generated/assets.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
@@ -6,8 +7,55 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        'HI'
+      body: Stack(
+        children: [
+          Image.asset(
+            Assets.assetsBg1,
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          Column(
+            children: [
+              Flexible(
+                flex: 5,
+                  child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('fhskaf', style: TextStyle(
+                              fontSize: 40
+                            ),),
+                            Text('fhskaf', style: TextStyle(
+                                fontSize: 40
+                            ),),
+                          ],
+                        ),
+                  )
+              ),
+              Flexible(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text('Press')
+                          )
+                      ),Expanded(
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text('Press')
+                          )
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
